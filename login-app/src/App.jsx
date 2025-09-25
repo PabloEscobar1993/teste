11061dcc6b9b312d27e29b6195c3bf4286852259
Pablo Rafael <pablo7r@outlook.com>
2025-09-25 09:06:44 -0300

    Tela de login

---
+++ login-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginScreen from './LoginScreen'; // Certifique-se de que o caminho do arquivo está correto
+
+function App() {
+  return (
+    <div className="App">
+      <LoginScreen />
+    </div>
+  );
+}
+
+export default App;
